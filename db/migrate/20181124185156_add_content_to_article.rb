7d5366a0735bf6e2f94170a6d40ea21094c152f0
Jonathan <jon1969edwards@gmail.com>
2018-11-25 17:19:41 +0100

    Modifies migrate file title to match class

--- db/migrate/20181124185156_add_content_to_article.rb
+++ db/migrate/20181124185156_add_content_to_article.rb
@@ -1,4 +1,4 @@
-class AddFieldNameToTableName < ActiveRecord::Migration[5.2]
+class AddContentToArticle < ActiveRecord::Migration[5.2]
   def change
     add_column :table_names, :field_name, :data_type
   end
